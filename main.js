3a174764596eb787674c048842ed1e1c04ebc89e
Krishna Sivapothanathan <krishnasivapothanathan@gmail.com>
2023-12-22 20:59:52 +0530

    js file updated

---
+++ main.js
@@ -0,0 +1,11 @@
+// Grab elements
+
+// Nav styles on scroll
+
+// Open menu & search form pop-up
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
