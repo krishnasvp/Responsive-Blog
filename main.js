0fbc2685d4493bf36fc945b64cf2502da22c9a27
Krishna Sivapothanathan <krishnasivapothanathan@gmail.com>
2023-12-22 21:12:07 +0530

    grab elements feature added

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // Grab elements
+const selectElement = selector =>{
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error('Something went wrong, make sure that ${selector} exists or is typed correctly.');
+};
 
 // Nav styles on scroll
 
